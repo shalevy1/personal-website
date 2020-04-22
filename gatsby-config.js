6758baadcf830838eca079c6d645613ddbdf9860
Sarah Abderemane <sarahabderemane@gmail.com>
2020-04-22 03:56:42 +0200

    add dark mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,7 @@ module.exports = {
             }
             `,
             output: '/rss.xml',
-            title: 'Gatsby RSS feed',
+            title: 'Sarah Abderemane RSS feed',
           },
         ],
       },
@@ -133,11 +133,45 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    `gatsby-plugin-dark-mode`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `http://sarah-abderemane.fr/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-vscode`,
+            // All options are optional. Defaults shown here.
+            options: {
+              theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: '', // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [], // Third-party extensions providing additional themes and languages
+              languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({
+                // Function allowing dynamic setting of additional class names on individual lines
+                content, //   - the string content of the line
+                index, //   - the zero-based index of the line within the code fence
+                language, //   - the language specified for the code fence
+                meta, //   - any options set on the code fence alongside the language (more on this later)
+              }) => '',
+              logLevel: 'warn', // Set to 'info' to debug if something looks wrong
+            },
+          },
+        ],
       },
     },
   ],
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import { Link } from 'gatsby'
 
 class Header extends React.Component {
@@ -77,7 +78,59 @@ class Header extends React.Component {
             </Link>
           </nav>
           <div>
-            <button className="toggle-theme" onClick={() => this.toggleMode()}>
+            <ThemeToggler>
+              {({ theme, toggleTheme }) => (
+                <label style={{ display: `flex` }}>
+                  <input
+                    style={{ opacity: 0 }}
+                    type="checkbox"
+                    onChange={e =>
+                      toggleTheme(e.target.checked ? 'dark' : 'light')
+                    }
+                    checked={theme === 'dark'}
+                  />{' '}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-sun"
+                    display={theme === 'dark' ? 'block' : 'none'}
+                  >
+                    <circle cx="12" cy="12" r="5"></circle>
+                    <line x1="12" y1="1" x2="12" y2="3"></line>
+                    <line x1="12" y1="21" x2="12" y2="23"></line>
+                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                    <line x1="1" y1="12" x2="3" y2="12"></line>
+                    <line x1="21" y1="12" x2="23" y2="12"></line>
+                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-moon"
+                    display={theme === 'dark' ? 'none' : 'block'}
+                  >
+                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                  </svg>
+                </label>
+              )}
+            </ThemeToggler>
+            {/* <button className="toggle-theme" onClick={() => this.toggleMode()}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -116,7 +169,7 @@ class Header extends React.Component {
               >
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
               </svg>
-            </button>
+            </button> */}
           </div>
         </div>
       </header>
