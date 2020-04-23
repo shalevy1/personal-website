2599527cf60fa304a54662d8825da0ba3f1b7f43
Sarah Abderemane <sarahabderemane@gmail.com>
2020-04-24 00:17:30 +0200

    fix: remove theme vscode plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Sarah Abderemane`,
     author: `Sarah Abderemane`,
     description: `Sarah Abderemane's Blog`,
-    siteUrl: `http://sarah-abderemane.fr/`,
+    siteUrl: `http://sarahabd.com/`,
     social: {
       twitter: `sabderemane`,
     },
@@ -58,7 +58,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-164223991-1`,
       },
     },
     {
@@ -138,40 +138,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
-        siteUrl: `http://sarah-abderemane.fr/`,
+        siteUrl: `http://sarahabd.com/`,
       },
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-vscode`,
-            // All options are optional. Defaults shown here.
-            options: {
-              theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
-              wrapperClassName: '', // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
-              injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
-              extensions: [], // Third-party extensions providing additional themes and languages
-              languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
-              replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-              getLineClassName: ({
-                // Function allowing dynamic setting of additional class names on individual lines
-                content, //   - the string content of the line
-                index, //   - the zero-based index of the line within the code fence
-                language, //   - the language specified for the code fence
-                meta, //   - any options set on the code fence alongside the language (more on this later)
-              }) => '',
-              logLevel: 'warn', // Set to 'info' to debug if something looks wrong
-            },
-          },
-        ],
       },
     },
   ],
