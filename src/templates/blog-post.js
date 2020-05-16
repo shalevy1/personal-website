5b3166caec2be8ddfc922c77cf861204882af665
Sarah Abderemane <sarahabderemane@gmail.com>
2020-05-16 15:40:17 +0200

    fix: subtitle blog mobile

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -28,21 +28,23 @@ class BlogPostTemplate extends React.Component {
             marginTop: rhythm(-1),
           }}
         >
-          {post.frontmatter.date} -{' '}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-clock"
-          >
-            <circle cx="12" cy="12" r="10"></circle>
-            <polyline points="12 6 12 12 16 14"></polyline>
-          </svg>{' '}
-          {post.fields.readingTime.text}
+          <span>{post.frontmatter.date}</span> <span className="dash">-</span>
+          <span className="time">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-clock"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <polyline points="12 6 12 12 16 14"></polyline>
+            </svg>{' '}
+            {post.fields.readingTime.text}
+          </span>
         </small>
         <MDXRenderer wrapper="div">{post.body}</MDXRenderer>
         <hr
